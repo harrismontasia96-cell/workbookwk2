211bfe967339fecf42a4c50b70d201d8348f6f6e
montasia harris <mharris@my.yearupunited.com>
2025-09-29 11:15:24 -0400

    workbkwk2 string exercise fullname complete

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,7 +1,31 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FullNameApplication {
     public static void main(String[] args) {
+        Scanner myscanner = new Scanner(System.in);
+
+        System.out.println("please Enter your name: ");
+        System.out.print("first name");
+                String firstname = myscanner.nextLine();
+
+        System.out.print("Middle name");
+                String middlename = myscanner.nextLine();
+
+        System.out.print("Last name");
+                String lastname = myscanner.nextLine();
+
+                System.out.print("Suffix");
+                String suffix = myscanner.nextLine();
+
+                System.out.println(firstname + " " + middlename + " " + lastname + " " + suffix);
+
+
+
+
+
+
 
     }
 }
